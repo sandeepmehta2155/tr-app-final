6cf49b34f32a80fba89f9646ba4e4a104dfb603e
Sandeep Mehta <sandeep.mehta@bitontree.com>
2022-01-23 00:36:43 +0530

    Link added

--- Components/Layout/Header/HeaderComponent.tsx
+++ Components/Layout/Header/HeaderComponent.tsx
@@ -1,35 +1,57 @@
-import React from 'react'
-import HeaderStyle from './Header.module.scss' 
+import React from "react";
+import HeaderStyle from "./Header.module.scss";
 import Link from "next/link";
 
 export default function HeaderComponent() {
-    return (
-        <header className={HeaderStyle.header}>
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-                        <div className="ast_logo  float-start w-100 py-3">
-                            <a href="https://trivediravi.com"><img src="https://trivediravi.com/wp-content/themes/astro/images/header/logo.png" alt="Logo" title="Logo" /></a>
-                            <button className="ast_menu_btn d-none"><i className="fa fa-bars" aria-hidden="true"></i></button>
-                        </div>
-                    </div>
-                    <div className="col-lg-9 col-md-9 col-sm-9 col-xs-12">
-                        <div className="ast_main_menu_wrapper">
-                            <div className="ast_menu w-100 float-start position-relative">
-                                <ul>
-                                    <li ><Link href="/">home</Link></li>
-                                    <li  ><Link href="/about">about</Link></li>
-                                    <li><a href="https://trivediravi.com/my-calendar/">Calendar</a></li>
-                                    <li><Link href="/services">services</Link></li>
-                                    <li><a href="/blog">blog</a></li>
-                                    <li><a href="/appointment">appointment</a></li>
-                                    <li><Link href="/contact">contact-us</Link></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                {/* <nav className="navbar navbar-expand-lg navbar-light">
+  return (
+    <header className={HeaderStyle.header}>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+            <div className="ast_logo  float-start w-100 py-3">
+              <a href="https://trivediravi.com">
+                <img
+                  src="https://trivediravi.com/wp-content/themes/astro/images/header/logo.png"
+                  alt="Logo"
+                  title="Logo"
+                />
+              </a>
+              <button className="ast_menu_btn d-none">
+                <i className="fa fa-bars" aria-hidden="true"></i>
+              </button>
+            </div>
+          </div>
+          <div className="col-lg-9 col-md-9 col-sm-9 col-xs-12">
+            <div className="ast_main_menu_wrapper">
+              <div className="ast_menu w-100 float-start position-relative">
+                <ul>
+                  <li>
+                    <Link href="/">home</Link>
+                  </li>
+                  <li>
+                    <Link href="/about">about</Link>
+                  </li>
+                  <li>
+                    <a href="https://trivediravi.com/my-calendar/">Calendar</a>
+                  </li>
+                  <li>
+                    <Link href="/services">services</Link>
+                  </li>
+                  <li>
+                    <Link href="/blog">blog</Link>
+                  </li>
+                  <li>
+                    <Link href="/appointment">appointment</Link>
+                  </li>
+                  <li>
+                    <Link href="/contact">contact-us</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <nav className="navbar navbar-expand-lg navbar-light">
                     <div className={HeaderStyle.logo}>
                         <a href="#">
                             <img src="/assets/Color-Logo.png" />
@@ -62,7 +84,7 @@ export default function HeaderComponent() {
                         </ul>
                     </div>
                 </nav> */}
-            </div>
-        </header>
-    )
+      </div>
+    </header>
+  );
 }
